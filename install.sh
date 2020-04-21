e15a92409ebb708a831cbc52e933daf93f005d19
Roberto Rolo <robertorolo@gmail.com>
2020-04-21 20:10:07 -0300

    added suckles

--- install.sh
+++ install.sh
@@ -9,3 +9,15 @@ sudo chmod +x ~/.xinitrc ~/.local/bin/*
 
 echo Installing packages
 sudo pacman -S $(cat progs)
+
+echo Compiling suckless software
+cd .local/src/dwm
+sudo make clean Install
+cd ..
+cd st 
+sudo make clean install
+cd ..
+cd dmenu
+sudo make clean install
+
+echo We are ready to go
