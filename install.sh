d20a640e9a7cf54929dd638fbefc268d2afa5092
Roberto Rolo <robertorolo@gmail.com>
2020-04-21 17:59:58 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo Installing packages
-sudo pacman -S $(cat progs)
-
 echo copying dotfiles to home directory
 cp .xinitrc .bashrc ~
 cp -rp .local .config ~
 
 echo giving permissions
 sudo chmod +x ~/.xinitrc ~/.local/bin/*
+
+echo Installing packages
+sudo pacman -S $(cat progs)
