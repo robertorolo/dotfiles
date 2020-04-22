02433480be952516e563a4c51d1648acf098e3f8
Roberto Rolo <robertorolo@gmail.com>
2020-04-22 18:47:15 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo copying dotfiles to home directory
-cp .xinitrc .bashrc .Xresources .vimrc ~
+cp .xinitrc .bashrc .Xresources .vimrc .gitconfig ~
 cp -rp .local .config ~
 
 echo giving permissions
