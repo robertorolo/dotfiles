facc5bd896eab5c311da5632588da365a0ce43ab
Roberto Rolo <robertorolo@gmail.com>
2020-12-20 12:25:31 -0300

    alacrity and xinit

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "Installing programs..."
 sleep 1
 
-apt-get install lightdm openbox obconf obmenu lxterminal leafpad firefox
+apt-get install xinit openbox obconf obmenu alacrity leafpad firefox
 
 path=$(pwd)
 
