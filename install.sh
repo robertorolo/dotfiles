d1af83fe78c1665702898c688cd2041095a0cbd0
Roberto Rolo <robertorolo@gmail.com>
2020-12-20 12:09:19 -0300

    openbox configs first commit

--- install.sh
+++ install.sh
@@ -1,48 +1,6 @@
 #!/bin/bash
 
-echo copying dotfiles to home directory
-cp .xinitrc .bashrc .Xresources .vimrc .gitconfig ~
-cp -rp .local .config ~
+echo "Installing programs..."
+sleep 1
 
-echo copying other config files
-sudo cp 20-intel.conf /etc/X11/xorg.conf.d
-
-echo giving permissions
-sudo chmod +x ~/.xinitrc ~/.bashrc ~/.local/bin/* ~/.config/sxiv/exec/key-handler
-
-echo Installing packages
-sudo pacman -S $(cat progs)
-
-echo Installing yay
-mkdir ~/.local/src
-cd ~/.local/src
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-
-echo Installing yay packages
-yay -S $(cat ~/dotfiles/progs_aur)
-
-echo Compiling suckless software
-cd ..
-git clone https://github.com/robertorolo/dwm.git
-cd dwm
-sudo make clean install
-cd ..
-git clone https://github.com/LukeSmithxyz/st.git
-cd st
-sudo make clean install
-cd ..
-git clone git://git.suckless.org/dmenu
-cd dmenu
-sudo make clean install
-
-echo Enabling services
-sudo systemctl enable bluetooth.service
-sudo systemctl start bluetooth.service
-sudo echo "AutoEnable=true" >> /etc/bluetooth/main.conf
-
-sudo systemctl enable tlp.service
-sudo systemctl start tlp.service
-
-echo We are ready to go
+apt-get install lightdm openbox obconf obmenu lxterminal leafpad firefox  
