b0dfecd24d585f096fdb4a92d689990dd914e81a
Roberto Rolo <robertorolo@gmail.com>
2020-12-20 14:25:21 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ sleep 1
 
 apt-get install xinit openbox obconf obmenu lxterminal leafpad firefox pcmanfm tint2 nitrogen lxappearance compton rofi
 
+echo "copying dotfiles..."
+sleep 1
+
 path=$(pwd)
 
 ln -sf $path/.xinitrc $HOME/.xinitrc
-apt-get install xinit openbox obconf obmenu alacrity leafpad firefox  
