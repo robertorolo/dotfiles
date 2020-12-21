272c817fc2087236bfccab891a924a52bbbe380a
Roberto Rolo <robertorolo@gmail.com>
2020-12-21 16:41:01 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 echo "Installing programs..."
 sleep 1
 
-pacman -S xinit openbox obconf obmenu lxterminal leafpad firefox pcmanfm tint2 nitrogen lxappearance compton rofi 
+pacman -S xinit qtile kitty nitrogen
+
+pip3 install --user pywal
 
 echo "copying dotfiles..."
 sleep 1
@@ -14,13 +16,3 @@ ln -sf $path/.xinitrc $HOME/.xinitrc
 
 # Config directory
 [ ! -d $HOME/.config ] && mkdir $HOME/.config
-
-# openbox
-[ ! -d $HOME/.config/openbox ] && mkdir $HOME/.config/openbox
-ln -sf $path/autostart $HOME/.config/openbox/autostart
-ln -sf $path/rc.xml $HOME/.config/openbox/rc.xml
-ln -sf $path/menu.xml $HOME/.config/openbox/menu.xml
-
-# tint2
-[ ! -d $HOME/.config/tint2 ] && mkdir $HOME/.config/tint2
-ln -sf $path/tint2rc $HOME/.config/tint2/tint2rc
