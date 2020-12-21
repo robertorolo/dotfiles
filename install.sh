1e97951cc8bed289e44b9756bf177958da780c03
Roberto Rolo <robertorolo@gmail.com>
2020-12-21 10:11:48 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,11 @@
 echo "Installing programs..."
 sleep 1
 
-apt-get install xinit openbox obconf obmenu lxterminal leafpad firefox pcmanfm tint2 nitrogen lxappearance compton rofi
+apt-get install xinit openbox obconf obmenu lxterminal leafpad firefox pcmanfm tint2 nitrogen lxappearance compton rofi arc-theme 
+
+git clone https://github.com/horst3180/arc-icon-theme --depth 1 && cd arc-icon-theme
+./autogen.sh --prefix=/usr
+sudo make install
 
 echo "copying dotfiles..."
 sleep 1
