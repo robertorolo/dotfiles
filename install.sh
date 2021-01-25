ebc4368fe803f6898e7a6bbc7bda3b782134a8b8
Roberto Rolo <robertorolo@gmail.com>
2021-01-25 15:51:58 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ while true; do
     case $input in
         [yY]*)
             echo 'Installing xfce'
-		        pacman -S xorg xfce4 xfce4-goodies lightdm lightdm-gtk-greeter chromium network-manager-applet pulseaudio pavucontrol xf86-video-intel bluez blueman
+		        pacman -S xorg xfce4 xfce4-goodies lightdm lightdm-gtk-greeter chromium network-manager-applet pulseaudio pavucontrol xf86-video-intel bluez bluez-utils
 				
 		        systemctl enable lightdm
             break
