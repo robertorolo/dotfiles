5abb4a0b746765c8620cdffd60fed2851b1427c8
Roberto Rolo <robertorolo@gmail.com>
2021-01-31 10:16:22 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ while true; do
     case $input in
         [yY]*)
             echo 'Installing xfce'
-		        pacman -S xorg xfce4 xfce4-goodies lightdm lightdm-gtk-greeter chromium network-manager-applet pulseaudio pavucontrol xf86-video-intel bluez bluez-utils file-roller evince flameshot sublime-text
+		        pacman -S xorg xfce4 xfce4-goodies lightdm lightdm-gtk-greeter chromium network-manager-applet pulseaudio pavucontrol xf86-video-intel bluez bluez-utils file-roller evince flameshot sublime-text mpv
 				
 		        systemctl enable lightdm
 			systemctl enable bluetooth
