72dd439d5451d728a1aba90846f77c14ab134d12
Roberto Rolo <robertorolo@gmail.com>
2021-01-31 10:39:06 -0300

    update ibov script

--- scripts/ibov.py
+++ scripts/ibov.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/home/void/anaconda3/envs/drh/bin/python
 
 #a function to retrieve ibov value
 from yahoo_fin import stock_info as si
@@ -6,6 +6,6 @@ from yahoo_fin import stock_info as si
 def get_ibov():
     val = si.get_live_price('^BVSP')
     round_val = (val/1000).round(2)
-    return 'IBOV '+str(round_val)+'K'
+    return ' - IBOV '+str(round_val)+'K'
 
 print(get_ibov())
