38800bda99a65e21a2d0e55dd99158eb4615dcdd
Roberto Rolo <robertorolo@gmail.com>
2021-02-02 08:18:27 -0300

    selection to chose different des

--- install.sh
+++ install.sh
@@ -1,37 +1,47 @@
 #!/bin/bash
 
-while true; do
-    read -p 'Install xfce? yes/no: ' input
-    case $input in
-        [yY]*)
-		echo 'Installing xfce and some basic programs'
-		sudo pacman -S xorg xfce4 xfce4-goodies lightdm lightdm-gtk-greeter chromium network-manager-applet pulseaudio pavucontrol xf86-video-intel bluez bluez-utils file-roller evince flameshot sublime-text mpv transmission-gtk
+echo 'Instaling yay...'
+echo Installing yay
+mkdir ~/.local/src
+cd ~/.local/src
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 
-		sudo systemctl enable lightdm
-		sudo systemctl enable bluetooth
+echo "Which DE you want to install?"
 
-		echo 'Instaling yay'
-		echo Installing yay
-		mkdir ~/.local/src
-		cd ~/.local/src
-		git clone https://aur.archlinux.org/yay.git
-		cd yay
-		makepkg -si
+# Operating system names are used here as a data source
+select de in xfce gnome kde; do
 
-		echo 'Installing yay packages'
-		yay -S qogir-gtk-theme qogir-icon-theme ttf-jetbrains-mono ttf-ms-fonts ttf-ubuntu-font-family nord-xfce-terminal 
-            break
-            ;;
-        [nN]*)
-            echo 'Continue installation.'
-            exit 1
-            ;;
-         *)
-            echo 'Invalid input' >&2
+    case $de in
+    xfe)
+        echo "Installing xfce..."
+        echo 'Installing xfce and some basic programs'
+        sudo pacman -S xorg xfce4 xfce4-goodies lightdm lightdm-gtk-greeter chromium network-manager-applet pulseaudio pavucontrol xf86-video-intel bluez bluez-utils file-roller evince flameshot sublime-text mpv transmission-gtk
+
+        sudo systemctl enable lightdm
+        sudo systemctl enable bluetooth
+
+        yay -S qogir-gtk-theme qogir-icon-theme ttf-jetbrains-mono ttf-ms-fonts ttf-ubuntu-font-family nord-xfce-terminal 
+        break
+        ;;
+    gnome)
+        echo "Installing gnome..."
+        break
+        ;;
+    kde)
+        echo "Installing kde..."
+        break
+        ;;
+    *)
+        echo "Invalid entry."
+        ;;
     esac
+
 done
 
 echo "copying dotfiles..."
+cd ~
 sleep 1
 
 path=$(pwd)
--- scripts/ibov.py
+++ scripts/ibov.py
@@ -6,6 +6,6 @@ from yahoo_fin import stock_info as si
 def get_ibov():
     val = si.get_live_price('^BVSP')
     round_val = (val/1000).round(2)
-    return ' - IBOV '+str(round_val)+'K'
+    return ' - IBOV '+str(round_val)+'K - '
 
 print(get_ibov())
