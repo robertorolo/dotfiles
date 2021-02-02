32aa80b057079ce411c8fca712556c05630a46f0
Roberto Rolo <robertorolo@gmail.com>
2021-02-02 07:53:08 -0300

    yay

--- install.sh
+++ install.sh
@@ -4,11 +4,22 @@ while true; do
     read -p 'Install xfce? yes/no: ' input
     case $input in
         [yY]*)
-            echo 'Installing xfce'
-		        pacman -S xorg xfce4 xfce4-goodies lightdm lightdm-gtk-greeter chromium network-manager-applet pulseaudio pavucontrol xf86-video-intel bluez bluez-utils file-roller evince flameshot sublime-text mpv
-				
-		        systemctl enable lightdm
-			systemctl enable bluetooth
+		echo 'Installing xfce and some basic programs'
+		sudo pacman -S xorg xfce4 xfce4-goodies lightdm lightdm-gtk-greeter chromium network-manager-applet pulseaudio pavucontrol xf86-video-intel bluez bluez-utils file-roller evince flameshot sublime-text mpv transmission-gtk
+
+		sudo systemctl enable lightdm
+		sudo systemctl enable bluetooth
+
+		echo 'Instaling yay'
+		echo Installing yay
+		mkdir ~/.local/src
+		cd ~/.local/src
+		git clone https://aur.archlinux.org/yay.git
+		cd yay
+		makepkg -si
+
+		echo 'Installing yay packages'
+		yay -S qogir-gtk-theme qogir-icon-theme ttf-jetbrains-mono ttf-ms-fonts ttf-ubuntu-font-family nord-xfce-terminal 
             break
             ;;
         [nN]*)
