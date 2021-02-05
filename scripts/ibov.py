29a6b36e74a90170df86bd0a18aeca3ed7f55e89
Roberto Rolo <robertorolo@gmail.com>
2021-02-05 08:32:51 -0300

    add check connection to ibov

--- scripts/ibov.py
+++ scripts/ibov.py
@@ -1,6 +1,7 @@
 #!/home/void/anaconda3/envs/drh/bin/python
 
 #a function to retrieve ibov value
+import requests
 from yahoo_fin import stock_info as si
 
 def get_ibov():
@@ -8,4 +9,12 @@ def get_ibov():
     round_val = (val/1000).round(2)
     return ' IBOV '+str(round_val)+'K '
 
-print(get_ibov())
+url = "http://www.google.com"
+timetout = 5
+
+try:
+	request = requests.get(url, timeout=timetout)
+	print(get_ibov())
+
+except (requests.ConnectionError, requests.Timeout) as exception:
+	print('No connection')
