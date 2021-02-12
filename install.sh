62ea2acf4962f382c6ef8c17201289c5c8b08f5f
Roberto Rolo <robertorolo@gmail.com>
2021-02-11 23:57:58 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ select de in xfce gnome kde i3; do
     i3)
 	echo "Installing i3..."
 
-	sudo pacman -S xorg-server xorg-xinit xorg-xrdb xf86-video-intel i3-gaps i3status rxvt-unicode dmenu qutebrowser ranger alsa-utils feh python-pywal
+	sudo pacman -S xorg-server xorg-xinit xorg-xrdb xf86-video-intel i3-gaps i3status rxvt-unicode dmenu qutebrowser ranger alsa-utils feh python-pywal xorg-xbacklight
 	
 	yay -S polybar
 	
