1d99180a53e25a8147ffc5d0bda7e32affade4dd
Roberto Rolo <robertorolo@gmail.com>
2021-02-11 23:50:41 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,7 +50,9 @@ select de in xfce gnome kde i3; do
     i3)
 	echo "Installing i3..."
 
-	sudo pacman -S xorg-server xorg-xinit xorg-xrdb xf86-video-intel i3-gaps i3status rxvt-unicode dmenu qutebrowser ranger alsa-utils feh python-pywal polybar
+	sudo pacman -S xorg-server xorg-xinit xorg-xrdb xf86-video-intel i3-gaps i3status rxvt-unicode dmenu qutebrowser ranger alsa-utils feh python-pywal
+	
+	yay -S polybar
 	
 	echo "copying dotfiles..."
 
@@ -65,6 +67,10 @@ select de in xfce gnome kde i3; do
 	#i3
 	[ ! -d $HOME/.config/i3 ] && mkdir $HOME/.config/i3
 	cp $path/config $HOME/.config/i3
+	
+	#polybar
+	[ ! -d $HOME/.config/polybar ] && mkdir $HOME/.config/polybar
+	cp $path/launch.sh $HOME/.config/polybar
 	
 	#fonts
 	[ ! -d $HOME/.config/fontconfig ] && mkdir $HOME/.config/fontconfig
