ef101c71850fd394837af1060f9e1b08a6339e35
Roberto Rolo <robertorolo@gmail.com>
2021-02-12 16:33:34 -0300

    added scrot to list

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ select de in xfce gnome kde i3; do
     i3)
 	echo "Installing i3 and some basic progrmas..."
 
-	sudo pacman -S xorg-server xorg-xinit xorg-xrdb xf86-video-intel i3-gaps i3status rxvt-unicode dmenu qutebrowser ranger alsa-utils pulseaudio feh python-pywal xorg-xbacklight dunst libnotify
+	sudo pacman -S xorg-server xorg-xinit xorg-xrdb xf86-video-intel i3-gaps i3status rxvt-unicode dmenu qutebrowser ranger alsa-utils pulseaudio feh python-pywal xorg-xbacklight dunst libnotify scrot
 	
 	yay -S polybar
 	
