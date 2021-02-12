c2af611e36b2cbdecb3d09b9f8ef73a8ccaafb61
Roberto Rolo <robertorolo@gmail.com>
2021-02-12 00:15:35 -0300

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -7,7 +7,6 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
