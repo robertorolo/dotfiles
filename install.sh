ad8de08c8cac1c9a5ae37f64ff90a1d61f2713ed
Roberto Rolo <robertorolo@gmail.com>
2021-02-11 21:22:34 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -51,8 +51,6 @@ select de in xfce gnome kde i3; do
 	sudo pacman -S xorg-server xorg-xinit xf86-video-intel i3-gaps i3status rxvt-unicode dmenu qutebrowser ranger alsa-utils feh
 
 	echo "copying dotfiles..."
-	cd ~
-	sleep 1
 
 	path=$(pwd)
 
