fedfb0416aab425c620dc41b30e8a973f9ae0f38
Roberto Rolo <robertorolo@gmail.com>
2021-02-12 10:44:00 -0300

    polybar again

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar mybar 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
