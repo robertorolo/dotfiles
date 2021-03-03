75a7ec58548e26700a8a983ebff1f6136acfca4b
Roberto Rolo <robertorolo@gmail.com>
2021-03-02 23:42:45 -0300

    more quetebrowser confs

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -4,6 +4,13 @@ c.downloads.position = 'bottom'
 #c.spellcheck.languages = ['en-US', 'pt-BR']
 c.url.start_pages = "https://robertorolo.github.io"
 
-c.url.searchengines = {"DEFAULT": "https://www.google.com.ar/search?q={}", "wa":"https://wiki.archlinux.org/?search={}", 're':'https://www.reddit.com/search?q={}'}
+c.url.searchengines = {"DEFAULT": "https://www.google.com.ar/search?q={}",
+                       "wa":"https://wiki.archlinux.org/?search={}",
+                       're':'https://www.reddit.com/search?q={}', 
+                       'yt':'https://www.youtube.com/results?search_query={}'}
 
 config.source('gruvbox.py')
+
+config.bind('f', 'hint all tab')
+config.bind('F', 'hint')
+config.bind('p', 'spawn mpv {url}')
