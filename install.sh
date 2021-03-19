495ccf5f967c4762d50ac8caf74bff81b2bb63bc
Roberto Rolo <robertorolo@gmail.com>
2021-03-18 23:32:49 -0300

    fix some colors and alphas

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ select de in yes no; do
 		yes)
 		echo "Installing i3 and some of your system dependencies..."
 		sudo pacman -Syuu
-		sudo pacman -S base-devel xorg-server xorg-xinit xorg-xrdb xf86-video-intel i3-gaps rxvt-unicode rofi ranger alsa-utils pulseaudio feh python-pywal xorg-xbacklight dunst libnotify scrot w3m xorg-xinput pcmanfm neovim xsel lxappearance otf-ipafont noto-fonts-emoji ttf-roboto-mono python-pip xclip xorg-xrdb mpv youtube-dl python-adblock zathura zathura-pdf-mupdf
+		sudo pacman -S base-devel xorg-server xorg-xinit xorg-xrdb xf86-video-intel i3-gaps rxvt-unicode rofi ranger alsa-utils pulseaudio feh python-pywal xorg-xbacklight dunst libnotify scrot w3m xorg-xinput pcmanfm neovim xsel lxappearance otf-ipafont noto-fonts-emoji ttf-roboto-mono python-pip xclip xorg-xrdb mpv youtube-dl python-adblock zathura zathura-pdf-mupdf transmission-cli
 		
 		echo 'Instaling yay...'
 		mkdir ~/.local
