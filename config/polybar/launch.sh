58020f28df9c5080a43c25547e13e2e3f30de74b
Roberto Rolo <robertorolo@gmail.com>
2021-04-08 21:22:37 -0300

    fixed dual monitors script

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -8,5 +8,6 @@ killall -q polybar
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
 polybar mybar 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar mybar1 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
