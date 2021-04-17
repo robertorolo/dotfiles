804c9a24831b5136182a13164b2da8eef61d498b
Roberto Rolo <robertorolo@gmail.com>
2021-04-17 11:59:41 -0300

    notifications on record screen

--- scripts/recordmyscreen.sh
+++ scripts/recordmyscreen.sh
@@ -2,5 +2,5 @@
 
 OUTFL="$1"
 
-ffmpeg -video_size 1366x768 -framerate 25 -f x11grab -i :0.0 -f pulse -ac 2 -i 0 $OUTFL.mp4
+notify-send "Recording my screen!" && ffmpeg -video_size 1366x768 -framerate 25 -f x11grab -i :0.0 -f pulse -ac 2 -i 0 $OUTFL.mp4 
 
